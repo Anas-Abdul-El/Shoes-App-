11cba5d41d54196e401947d1d21cbcfce5781d49
Anas-Abdul-El <anasabdulel@gmail.com>
2026-01-31 17:56:15 +0200

    add

---
+++ src/components/setting/admin-setting.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function adminSetting() {
+    return (
+        <div>adminSetting</div>
+    )
+}
+
+export default adminSetting
---
+++ src/components/setting/user-setting.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function userSetting() {
+    return (
+        <div>userSetting</div>
+    )
+}
+
+export default userSetting
