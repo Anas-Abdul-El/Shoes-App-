00ec627e838047615f6ce328d903b68ee59d8792
Anas-Abdul-El <anasabdulel@gmail.com>
2026-02-04 18:33:38 +0200

    fix error

--- src/components/cart/Cart-wr.tsx
+++ src/components/cart/Cart-wr.tsx
@@ -50,49 +50,49 @@ function CartWr({
         })
     }
 
-    if (products.length === 0) return <EmptyCart />
-
     return (
         <>
             {submit && <PayementAlert />}
-            <div className='bg-zinc-900 w-9/10 md:h-9/13 h-10/12 rounded-xl flex flex-col justify-start items-center p-4 box-border overflow-auto animate-slide-up'>
-                {
-                    products.map((product, index) => {
-                        return (
-                            <div key={product.id} className={`w-full md:h-24 h-30 bg-zinc-800 rounded-md mb-4 flex justify-start items-center p-4 box-border border border-zinc-700 transition-all duration-500 ${itemsVisible[index] ? 'animate-slide-up opacity-100' : 'opacity-0 translate-y-4'}`}>
-                                <div className='h-full w-full flex justify-between items-center'>
-                                    <div className='flex justify-start items-center h-full'>
-                                        <img src={product.imageUrl} alt={product.name} className='w-16 h-16 object-cover rounded-md mr-4' />
-                                        <div className='flex flex-col justify-between h-full'>
-                                            <h2 className='text-white font-bold italic'>{product.name}</h2>
-                                            <div className='flex flex-col md:flex-row space-x-8'>
-                                                <p className='text-white'>
-                                                    <span className='text-green-500'>price</span> : <span className='italic'>{product.price}</span>$
-                                                </p>
-                                                <p>Quantity : {product.cardQuantity}</p>
+            {products.length !== 0 ? (<>
+                <div className='bg-zinc-900 w-9/10 md:h-9/13 h-10/12 rounded-xl flex flex-col justify-start items-center p-4 box-border overflow-auto animate-slide-up'>
+                    {
+                        products.map((product, index) => {
+                            return (
+                                <div key={product.id} className={`w-full md:h-24 h-30 bg-zinc-800 rounded-md mb-4 flex justify-start items-center p-4 box-border border border-zinc-700 transition-all duration-500 ${itemsVisible[index] ? 'animate-slide-up opacity-100' : 'opacity-0 translate-y-4'}`}>
+                                    <div className='h-full w-full flex justify-between items-center'>
+                                        <div className='flex justify-start items-center h-full'>
+                                            <img src={product.imageUrl} alt={product.name} className='w-16 h-16 object-cover rounded-md mr-4' />
+                                            <div className='flex flex-col justify-between h-full'>
+                                                <h2 className='text-white font-bold italic'>{product.name}</h2>
+                                                <div className='flex flex-col md:flex-row space-x-8'>
+                                                    <p className='text-white'>
+                                                        <span className='text-green-500'>price</span> : <span className='italic'>{product.price}</span>$
+                                                    </p>
+                                                    <p>Quantity : {product.cardQuantity}</p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className=''>
-                                        <Button
-                                            variant="ghost"
-                                            size="icon"
-                                            onClick={() => handleDelete(product.id)}
-                                            className='cursor-pointer'>
-                                            <Trash2 className="w-4 h-4" />
-                                        </Button>
+                                        <div className=''>
+                                            <Button
+                                                variant="ghost"
+                                                size="icon"
+                                                onClick={() => handleDelete(product.id)}
+                                                className='cursor-pointer'>
+                                                <Trash2 className="w-4 h-4" />
+                                            </Button>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                }
-                <Button
-                    onClick={handleCheckout}
-                    className='bg-white w-40 h-15 fixed mx-auto bottom-9 rounded-md flex justify-center items-center cursor-pointer text-black font-bold hover:text-white'>
-                    Checkout
-                </Button>
-            </div>
+                            )
+                        })
+                    }
+                    <Button
+                        onClick={handleCheckout}
+                        className='bg-white w-40 h-15 fixed mx-auto bottom-9 rounded-md flex justify-center items-center cursor-pointer text-black font-bold hover:text-white'>
+                        Checkout
+                    </Button>
+                </div>
+            </>) : <EmptyCart />}
         </>
     )
 }
